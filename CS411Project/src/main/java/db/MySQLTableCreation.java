52de8d57d4b57fba93bf29bc6c240855124381a1
Yichi Zhang <zhangyichi9604@gmail.com>
2020-11-01 20:40:19 -0600

    push environment setup

---
+++ CS411Project/src/main/java/db/MySQLTableCreation.java
@@ -0,0 +1,48 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class MySQLTableCreation {
+    // Run this as Java application to reset the database.
+    public static void main(String[] args) {
+        try {
+            // Step 1 Connect to MySQL.
+            System.out.println("Connecting to " + MySQLDBUtil.URL);
+            Class.forName("com.mysql.cj.jdbc.Driver").getConstructor().newInstance();
+            Connection conn = DriverManager.getConnection(MySQLDBUtil.URL);
+
+            if (conn == null) {
+                return;
+            }
+
+            // Step 2 Drop tables in case they exist.
+            Statement statement = conn.createStatement();
+            String sql = "DROP TABLE IF EXISTS Test";
+            statement.executeUpdate(sql);
+
+            // Step 3 Create new tables
+            sql = "CREATE TABLE Test ("
+                    + "test_id INT NOT NULL,"
+                    + "first_name VARCHAR(255),"
+                    + "last_name VARCHAR(255),"
+                    + "PRIMARY KEY (test_id)"
+                    + ")";
+            statement.executeUpdate(sql);
+
+            // Step 4: insert fake user for test
+            sql = "INSERT INTO Test VALUES(1, 'Yichi', 'Zhang')";
+            statement.executeUpdate(sql);
+
+            sql = "INSERT INTO Test VALUES(2, '123', '456')";
+            statement.executeUpdate(sql);
+
+            conn.close();
+            System.out.println("Import done successfully");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ CS411Project/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: tonyzyc
+  Date: 10/25/20
+  Time: 00:33
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>CS411 Project</title>
+  </head>
+  <body>
+  <h1>Welcome to CS411 Project</h1>
+  </body>
+</html>
